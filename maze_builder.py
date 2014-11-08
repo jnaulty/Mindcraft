037283c1daf2e48c6cab757c5d7b5fd3e3359cd1
jnaulty <jnaulty@gmail.com>
2014-11-07 20:46:27 -0800

    maze_builder.py works!!!

--- maze_builder.py
+++ maze_builder.py
@@ -12,7 +12,7 @@ mc.postToChat("Begin Building Maze")
 
 
 #grab maze file 
-with open('maze2.txt', 'r') as f:
+with open('maze.txt', 'r') as f:
   data = f.read()
 f.close()
 
@@ -26,7 +26,7 @@ def check_block(location, BLOCK):
   #return Boolean if location has block.block
   #location = (x, y, z)
   #block = mcpi block type in CAPS
-  if mc.getBlock(location[0], location[1], location[2]) == block.BLOCK:
+  if mc.getBlock(location[0], location[1], location[2]) == BLOCK:
     return True
   else:
     return False
@@ -46,7 +46,8 @@ def build_maze(layer):
   #then use origin.x, origin.y, origin.z to set
   x,y,z = mc.player.getTilePos()
   lines = layer.split("\n")
-
+  x_origin = x
+  z_origin = z
   z_lines = len(lines)
   x_lines = len(lines[0])
   midline = x_lines//2
@@ -56,23 +57,28 @@ def build_maze(layer):
   mc.player.setTilePos(midline, y, z-1)
   
   for line in lines:
+    x_origin=0	
     for char in line:
       if char == "1":
         #set block where 1 goes
-        mc.setBlocks(x, y, z, x, y+2, z,  block.DIAMOND_BLOCK)
+        mc.setBlocks(x_origin, y, z, x_origin, y+2, z,  block.DIAMOND_BLOCK)
         #add one to the origin along x-axis
-        x+=1
+	mc.postToChat(x)
+      else:
+	char == "0"
+	mc.setBlocks(x_origin, y, z, x_origin, y+2, z, block.AIR)
+      x_origin+=1
     #add one to the origin along z-axis
     z+=1
   new_pos = cur_pos()
   new_pos[2]+=1 
   #make a doorway to maze
-  if not check_block(new_pos, AIR):
+  if not check_block(new_pos, block.AIR):
     mc.setBlocks(new_pos[0], new_pos[1], new_pos[2], new_pos[0], new_pos[1]+2, new_pos[2]+1, block.AIR)  
       
 
         
-build_layer(data)
+build_maze(data)
 
 
 
